4adf7fb23fb881b17dab4c34bcd50ee30a6730f1
Jakub Klimczak <jakub.klimczak.91@gmail.com>
2020-10-27 10:51:19 +0100

    Setting up.

---
+++ src/components/Header/header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./header.scss";
+
+export const Header = () => {
+    return (
+        <header className="App-header">
+            <h1>Hello World</h1>
+        </header>
+    );
+}
---
+++ src/layouts/App/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import React from "react";
+import {Header} from "../../components/Header/header";
+import {Footer} from "../../components/Footer/footer"
+
+function App() {
+  return (
+      <>
+        <Header />
+        <Footer />
+      </>
+  );
+}
+
+export default App;
