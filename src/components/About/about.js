5a00eb55f992f0b9706590b3e32dc4c9e1ddbbaa
Jakub Klimczak <jakub.klimczak.91@gmail.com>
2020-11-17 13:26:56 +0100

    Added link to about page.

--- src/components/About/about.js
+++ src/components/About/about.js
@@ -10,7 +10,7 @@ export const About = ({formName}) => {
                 <div className="about__container">
                     {width > 768 ? null : <span className="about__name">Hi, {formName}</span>}
                     <span className="about__header">About</span>
-                    <p className="about__text">This App was created as project on a JavaScript React programming course. If you like it or have questions, please contact me through contact page.</p>
+                    <p className="about__text">This App was created as a project on a JavaScript React programming course. If you like it or have questions, please contact me through contact page or Github <a href="https://github.com/Klimu2608" className="about__text--link">here</a>.</p>
                 </div>
             </section>
         </>
